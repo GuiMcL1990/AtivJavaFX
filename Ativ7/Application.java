726cc1280ddcb398cdbc146b0bb45224c5fd2e2b
GuiMcL1990 <guirvasco@gmail.com>
2025-03-17 14:20:03 -0300

    Add files via upload

---
+++ Ativ7/Application.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        VBox root = FXMLLoader.load(getClass().getResource("verificador_palindromo.fxml"));
+
+        Scene scene = new Scene(root, 300, 150);
+        primaryStage.setTitle("Verificador de Palíndromo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
