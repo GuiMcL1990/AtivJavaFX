47fb54bf7ca3d8ddf514133180ac0db1311c110c
GuiMcL1990 <guirvasco@gmail.com>
2025-03-17 14:19:16 -0300

    Add files via upload

---
+++ Ativ5/HelloApplication.java
@@ -0,0 +1,25 @@
+package com.parimpar.ativ5;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class HelloApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    
+        VBox root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setTitle("Simulador de Login");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
